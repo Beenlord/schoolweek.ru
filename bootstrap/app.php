b0265ca639ab44010b640e6693ad38a8baa8746d
Fomin VA <45092587+Beenlord@users.noreply.github.com>
2026-09-19 23:42:34 +0300

    Add home page files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: routes_path('web.php'),
         api: routes_path('api.php'),
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
 
         $middleware->alias([
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="Home HomePage">
+    </div>
+</template>
